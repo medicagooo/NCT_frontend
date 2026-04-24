3662380393b8aafbd63251c8e54c0a77f9e6ccf4
medicagooo <medicago.19@outlook.com>
2026-04-25 01:31:07 +0800

    fix:cors

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ module.exports = defineConfig({
     'process.env.NODE_ENV': JSON.stringify('production')
   },
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173
+  },
   build: {
     emptyOutDir: true,
     outDir: 'dist',
